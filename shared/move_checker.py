7da9da49566b6e90f66b786ae715537510796451
Christian Fiore <chrifiore12@gmail.com>
2024-11-09 17:14:15 +0100

    feat: implemented get_all_moves method

--- shared/move_checker.py
+++ shared/move_checker.py
@@ -1,5 +1,6 @@
-from .utils import State, _Action
-from typing import List
+from .utils import State, _Action, strf_square
+from typing import List, Tuple
+import numpy as np
 
 class MoveChecker:
     
@@ -9,7 +10,27 @@ class MoveChecker:
     
     @staticmethod
     def __get_all_moves(state: State) -> List[_Action]:
-        pass
+        all_actions = []
+        turn = state.turn
+        positions_of_movable_pieces: List[Tuple[int, int]] = list(zip(*(np.where(state.board.pieces == turn))))
+        
+        board_height = state.board.height
+        board_width = state.board.width
+        
+        for column, row in positions_of_movable_pieces:
+            for index in range(0, board_height + 1):
+                if index == row:
+                    continue
+                vertical_action = _Action(from_=strf_square((column, row)), to_=strf_square((column, index)), turn=turn)
+                all_actions.append(vertical_action)
+                
+            for index in range(0, board_width + 1):
+                if index == column:
+                    continue
+                horizontal_action = _Action(from_=strf_square((column, row)), to_=strf_square((index, row)), turn=turn)
+                all_actions.append(horizontal_action)
+                
+        return all_actions
     
     @classmethod
     def get_possible_moves(cls, state: State) -> List[_Action]:
