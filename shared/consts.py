0d5ad29e4f254ab43cdc8f09a824e0e3823ca58f
Christian Fiore <chrifiore12@gmail.com>
2024-11-10 18:01:57 +0100

    fix: adjusted camps positions

--- shared/consts.py
+++ shared/consts.py
@@ -1,3 +1,10 @@
 INITIAL_STATE = "OOOBBBOOO\nOOOOBOOOO\nOOOOWOOOO\nBOOOWOOOB\nBBWWKWWBB\nBOOOWOOOB\nOOOOWOOOO\nOOOOBOOOO\nOOOBBBOOO\n-\nW"
 
-CAMPS = [(3, 0), (4, 0), (5, 0), (4, 1), (0, 3), (0, 4), (0, 5), (3, 8), (4, 8), (5, 8), (1, 4), (8, 4), (8, 3), (8, 5), (4, 8)]
+CAMPS = {
+    (0, 3), (0, 4), (0, 5),
+    (1, 4),
+    (3, 0), (4, 0), (5, 0),
+    (8, 3), (8, 4), (8, 5),
+    (4, 1), (4, 8), (3, 8), (5, 8),
+    (8, 4)
+}
