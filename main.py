e7c80ddc08ca08e548ed15f839b65bd2ae5169a2
Christian Fiore <chrifiore12@gmail.com>
2024-11-14 15:52:01 +0100

    feat: setted main loop with random player

--- main.py
+++ main.py
@@ -1,34 +1,18 @@
 """
     Entrypoint for the TablutClient.
 """
-from shared.random_player import RandomPlayer
-from shared.utils import Color
-from dotenv import load_dotenv
 import os
-
-load_dotenv()
-
-
-"""
-# PLAYER_COLOR = os.environ['PLAYER_COLOR']
-# TIMEOUT = os.environ['TIMEOUT']
-SERVER_IP = os.environ['SERVER_IP']
-# WEBSOCKET_PORT = os.environ['WEBSOCKET_PORT']
-
+from shared.random_player import RandomPlayer
+from shared.utils import strp_color
+from shared.consts import INITIAL_STATE
 from connectors.client import Client
 
-white_player = RandomPlayer(color=Color.WHITE)
-black_player = RandomPlayer(color=Color.BLACK)
+PLAYER_COLOR = os.environ['PLAYER_COLOR']
+TIMEOUT = os.environ['TIMEOUT']
+SERVER_IP = os.environ['SERVER_IP']
+WEBSOCKET_PORT = os.environ['WEBSOCKET_PORT']
 
-white_client = Client(player=white_player, server_ip=SERVER_IP, port=5800)
-black_client = Client(player=black_player, server_ip=SERVER_IP, port=5801)
-
-white_client.send_name()
-black_client.send_name()
-
-white_client.read_state()
-white_client.send_move({"from": "e4", "to": "e5"})
-
-black_client.read_state()
-black_client.send_move({"from": "e7", "to": "e3"})
-"""
+if __name__ == '__main__':
+    player = RandomPlayer(color=strp_color(PLAYER_COLOR))
+    client = Client(player=player, server_ip=SERVER_IP, port=int(WEBSOCKET_PORT), current_state=INITIAL_STATE, timeout=int(TIMEOUT))
+    client.main()
