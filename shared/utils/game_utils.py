948c8dbd968840ccf7c012eb5eb2eabda423289d
Christian Fiore <chrifiore12@gmail.com>
2024-11-14 16:17:00 +0100

    fix: color and turn comparison

--- shared/utils/game_utils.py
+++ shared/utils/game_utils.py
@@ -340,9 +340,9 @@ class Board:
         
         if moving_piece not in (Piece.DEFENDER, Piece.ATTACKER, Piece.KING):
             raise ValueError(f"Cannot move {moving_piece} from {action.from_} to {action.to_}.")
-        if action.turn == Color.WHITE and moving_piece not in (Piece.DEFENDER, Piece.KING):
+        if action.turn.value == Color.WHITE.value and moving_piece not in (Piece.DEFENDER, Piece.KING):
             raise ValueError("Cannot move opponent's pieces.")
-        if action.turn == Color.BLACK and moving_piece != Piece.ATTACKER:
+        if action.turn.value == Color.BLACK.value and moving_piece != Piece.ATTACKER:
             raise ValueError("Cannot move opponent's pieces.")
         if from_indexes == (self.__height // 2, self.__width // 2) and moving_piece == Piece.KING:
             self.__pieces[from_indexes] = Piece.THRONE
