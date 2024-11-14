bfdc8b27d4989176ec98630e6f1dbff8323c8d42
Christian Fiore <chrifiore12@gmail.com>
2024-11-14 15:51:28 +0100

    fix: socket setup

--- connectors/client.py
+++ connectors/client.py
@@ -1,14 +1,8 @@
-import socket
-import json
-
-from shared.random_player import RandomPlayer
-
-
 """
 The `Client` class represents a client that connects to the server to play the game.
 It includes methods for sending the player's name, move, and receiving the current game state.
 Attributes:
-    player (RandomPlayer): The player that connects to the server.
+    player (AbstractPlayer): The player that connects to the server.
     timeout (int): The time limit for the connection.
     server_ip (str): The IP address of the server.
     port (int): The port number of the server.
@@ -22,100 +16,167 @@ Methods:
     read_state(): Reads the current game state from the server.
 """
 
+import socket
+import struct
+import json
+
+from shared.utils import AbstractPlayer, strp_state, state_decoder, Turn
+
 class Client:
-    def __init__(self, player: RandomPlayer, server_ip, port, current_state=None, timeout=60):
+    """
+    The `Client` class represents a client that connects to the server to play the game.
+    It includes methods for sending the player's name, move, and receiving the current game state.
+
+    Attributes:
+        player (AbstractPlayer): The player that connects to the server.
+        timeout (int): The time limit for the connection.
+        server_ip (str): The IP address of the server.
+        port (int): The port number of the server.
+        current_state (State): The current game state visible to the player.
+        player_socket (socket.socket): The socket connection to the server.
+
+    Methods:
+        connect() -> socket.socket: Connects to the server using the player's socket.
+        send_name(): Sends the player's name to the server.
+        send_move(action): Sends the player's move to the server.
+        compute_move() -> dict: Computes the player's move.
+        read_state(): Reads the current game state from the server.
+    """
+
+    def __init__(self, player: AbstractPlayer, server_ip: str, port: int, current_state=None, timeout: int = 60):
         """
         Initializes a Client instance.
-        :param player: The player that connects to the server.
-        :param server_ip: The IP address of the server.
-        :param port: The port number of the server.
-        :param current_state: The current game state visible to the player.
-        :param timeout: The time limit for the connection.
+
+        Args:
+            player (AbstractPlayer): The player instance that connects to the server.
+            server_ip (str): The IP address of the server.
+            port (int): The port number of the server.
+            current_state (optional): The current game state visible to the player.
+            timeout (int, optional): The time limit for the connection. Defaults to 60 seconds.
         """
         self.player = player
         self.timeout = timeout
         self.server_ip = server_ip
         self.port = port
-        self.current_state = current_state
-        self.player_socket = self.connect(player, server_ip, port)
+        self.current_state = strp_state(current_state) if current_state else None
+        self._connect()
 
-    def connect(self, player, server_ip, port) -> socket.socket:
+    def __del__(self):
         """
-        Connects to the server using the player's socket.
-        :param player: The player that connects to the server.
-        :param server_ip: The IP address of the server.
-        :param port: The port number of the server.
-        :return: The socket connection to the server.
+        Closes the socket connection when the Client instance is deleted.
+        """
+        if self.socket:
+            self.socket.close()
+
+    def _connect(self) -> socket.socket:
+        """
+        Establishes a connection to the server.
+
+        Returns:
+            socket.socket: The socket connection object.
         """
         try:
-            player_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            player_socket.settimeout(60)
-
-            print(f"Connecting to {server_ip}:{port} as {player}...")
-            player_socket.connect((server_ip, port))
+            print(f"Connecting to {self.server_ip}:{self.port} as {self.player.name}...")
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.socket.settimeout(self.timeout)
+            self.socket.connect((self.server_ip, self.port))
             print("Connection established!")
-
-            return player_socket
-
         except socket.timeout:
-            print(f"Connection to {server_ip}:{port} timed out.")
+            print(f"Connection to {self.server_ip}:{self.port} timed out.")
         except socket.gaierror:
-            print(f"Address-related error connecting to {server_ip}:{port}.")
+            print(f"Address-related error connecting to {self.server_ip}:{self.port}.")
         except ConnectionRefusedError:
-            print(f"Connection refused by the server at {server_ip}:{port}.")
+            print(f"Connection refused by the server at {self.server_ip}:{self.port}.")
         except socket.error as e:
-            print(f"Failed to connect to {server_ip}:{port} due to: {e}")
+            print(f"Failed to connect to {self.server_ip}:{self.port} due to: {e}")
 
-    def send_name(self):
+    def _send_name(self):
         """
         Sends the player's name to the server.
         """
         try:
-            name_bytes = (self.player.name + '\n').encode('utf-8')
-
-            self.player_socket.sendall(name_bytes)
-
-            print(f"Sent name '{self.player.name}' to server.")
+            self.socket.send(struct.pack('>i', len(self.player.name)))
+            self.socket.send(self.player.name.encode())
+            print(f"Declared name '{self.player.name}' to server.")
         except socket.error as e:
-            print(f"Failed to send name: {e}")
+            print(f"Failed to send name to the server: {e}")
 
-    def send_move(self, action):
+    def _send_move(self, action):
         """
         Sends the player's move to the server.
-        :param action: The player's move as a dictionary
+
+        Args:
+            action (dict): The player's move as a dictionary.
         """
         try:
-            action_json = json.dumps(action)
-            action_bytes = action_json.encode('utf-8') + b'\n'
+            action_str = str(action)
+            self.socket.send(struct.pack('>i', len(action_str)))
+            self.socket.send(action_str.encode())
+        except socket.error as e:
+            print(f"Failed to send move to the server: {e}")
 
-            self.player_socket.sendall(action_bytes)
-
-            print(f"Sent move '{action_json}' to server.")
-        except (socket.error, json.JSONDecodeError) as e:
-            print(f"Failed to send move: {e}")
-
-    def compute_move(self):
+    def _compute_move(self) -> dict:
         """
         Computes the player's move.
-        :return: The player's move as a dictionary
-        """
-        return {"move": "some_move"}  # Provisory mocked action
 
-    def read_state(self):
+        Returns:
+            dict: The player's computed move.
+        """
+        return self.player.fit(self.current_state)
+
+    def _read_state(self):
         """
         Reads the current game state from the server.
-        :return: The current game state visible to the player.
+
+        Returns:
+            State: The current game state visible to the player.
         """
         try:
-            state_data = self.player_socket.recv(1024)
-            if not state_data:
-                print("Received empty response from server.")
-                return None
-
-            state_json = state_data.decode('utf-8')
-            self.current_state = json.loads(state_json)
-            print(f"Received state '{self.current_state}' from server.")
+            len_bytes = struct.unpack('>i', self._recvall(4))[0]
+            current_state_server_bytes = self.socket.recv(len_bytes)
+            self.current_state = json.loads(current_state_server_bytes, object_hook=state_decoder)
         except (socket.error, json.JSONDecodeError) as e:
-            print(f"Failed to read state: {e}")
-            return None
+            print(f"Failed to read or decode the server response: {e}")
+            raise RuntimeError('Failed to decode server response')
 
+    def _recvall(self, n: int) -> bytes:
+        """
+        Helper function to receive `n` bytes or return None if EOF is hit.
+
+        Args:
+            n (int): The number of bytes to receive.
+
+        Returns:
+            bytes: The received bytes data.
+        """
+        data = b''
+        while len(data) < n:
+            packet = self.socket.recv(n - len(data))
+            if not packet:
+                return None
+            data += packet
+        return data
+
+    def main(self):
+        """
+        Main loop for the client to handle game state updates and send moves.
+        """
+        self._send_name()
+        
+        while True:
+            print("Reading state...")
+            self._read_state()
+            print(self.current_state)
+            
+            if self.current_state.turn in (Turn.DRAW, Turn.BLACK_WIN, Turn.WHITE_WIN):
+                print(f"Game ended...\nResult: {self.current_state.turn.value}")
+                return
+            
+            if self.current_state.turn.value == self.player.color.value:
+                print("Calculating move...")
+                action = self._compute_move()
+                print(f"Sending move:\n{action}")
+                self._send_move(action)
+                print("Action sent")
+            else:
+                print("Waiting for opponent's move...")
