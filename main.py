3447a12e0950265f2b7ef5a23e9afd6d318ea3b9
mpreda01 <mpreda01@gmail.com>
2024-11-20 14:09:00 +0100

    bug in main.py

--- main.py
+++ main.py
@@ -27,4 +27,85 @@ if __name__ == '__main__':
         client.main()
     except Exception as e:
         logger.error("An error occurred: %s", e)
-        
+
+
+
+from environment.tablut import Environment
+from shared.history import History, Match
+from shared.utils import AbstractPlayer
+from shared.utils.game_utils import Color, Action, Board
+from shared.heuristic import heuristic
+from shared.utils.env_utils import strp_state
+STATE1 = (
+    "OOOBBBOOO\n"
+    "OOOOBOOOO\n"
+    "OOOOWOOOO\n"
+    "BOOOWOOOB\n"
+    "BBWWKWWBB\n"
+    "BOOOWOOOB\n"
+    "OOOOWOOOO\n"
+    "OOOOBOOOO\n"
+    "OOOBBBOOO\n"
+    "-\n"
+    "W"
+)
+STATE2 = (
+    "OOOBBBOOO\n"
+    "OOOOBOOOO\n"
+    "OOOOWWOOO\n"
+    "BOOOWOOOB\n"
+    "BBWWKOWBB\n"
+    "BOOOWOOOB\n"
+    "OOOOWOOOO\n"
+    "OOOOBOOOO\n"
+    "OOOBBBOOO\n"
+    "-\n"
+    "B"
+)
+STATE3 = (
+    "OOOBBBOOO\n"
+    "OOOOBOOOO\n"
+    "OOOOWWOOO\n"
+    "BOOOWOOOB\n"
+    "BBWWKOWBB\n"
+    "BOOOWOOOB\n"
+    "OOOOWOOOO\n"
+    "OOOOOBOOO\n"
+    "OOOBBBOOO\n"
+    "-\n"
+    "W"
+)
+STATE4 = (
+    "OOOBBBOOO\n"
+    "OOOOBOOOO\n"
+    "OOOOWOOOO\n"
+    "BOOOWOOOB\n"
+    "BBWWKWWBB\n"
+    "BOOOWOOOB\n"
+    "OOOOWOOOO\n"
+    "OOOOOBOOO\n"
+    "OOOBBBOOO\n"
+    "-\n"
+    "B"
+)
+STATE5 = (
+    "OOOBBBOOO\n"
+    "OOOOBOOOO\n"
+    "OOOOWOOOO\n"
+    "BOOOWOOOB\n"
+    "BBWWKWWBB\n"
+    "BOOOWOOOB\n"
+    "OOOOWOOOO\n"
+    "OOOOBOOOO\n"
+    "OOOBBBOOO\n"
+    "-\n"
+    "W"
+)
+p1 = AbstractPlayer(STATE1, "testw", Color.WHITE)
+p2 = AbstractPlayer(STATE1, "testb", Color.BLACK)
+turns = [(STATE1, Action(from_= "f5", to_="f7", turn=Color.WHITE), heuristic(STATE1,  Action(from_= "f5", to_="f7", turn=Color.WHITE)))]
+turns.append((STATE2, Action(from_= "e2", to_="f2", turn=Color.BLACK), heuristic(STATE1,  Action(from_= "e2", to_="f2", turn=Color.BLACK))))
+turns.append((STATE3, Action(from_= "f7", to_="f5", turn=Color.WHITE), heuristic(STATE1,  Action(from_= "f7", to_="f5", turn=Color.WHITE))))
+turns.append((STATE4, Action(from_= "f2", to_="e2", turn=Color.BLACK), heuristic(STATE1,  Action(from_= "f2", to_="e2", turn=Color.BLACK))))
+his = History(Match(1, p1, p2, turns))
+env = Environment(Board(strp_state(STATE5)), STATE5, his)
