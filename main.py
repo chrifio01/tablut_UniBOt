e6a8841620ade2f8702ed634cd746106c8c3f7c8
Christian Fiore <chrifiore12@gmail.com>
2024-11-26 01:02:34 +0100

    hotfix(main): added DqnAgent

--- main.py
+++ main.py
@@ -3,29 +3,27 @@ Entrypoint for the TablutClient module.
 """
 
 import os
-from shared.random_player import RandomPlayer
-from shared.utils import strp_color, strp_state
+from shared.utils import strp_color
 from shared.consts import INITIAL_STATE
 from shared.loggers import logger
 from connectors.client import Client
 from model.player import DQNPlayer
 
 if __name__ == '__main__':
-    # try:
-    PLAYER_COLOR = os.environ['PLAYER_COLOR']
-    # TIMEOUT = os.environ['TIMEOUT']
-    # SERVER_IP = os.environ['SERVER_IP']
-    # WEBSOCKET_PORT = os.environ['WEBSOCKET_PORT']
+    try:
+        PLAYER_COLOR = os.environ['PLAYER_COLOR']
+        TIMEOUT = os.environ['TIMEOUT']
+        SERVER_IP = os.environ['SERVER_IP']
+        WEBSOCKET_PORT = os.environ['WEBSOCKET_PORT']
 
-    # settings = {
-    #     'current_state': INITIAL_STATE,
-    #     'timeout': int(TIMEOUT),
-    #     'server_ip': SERVER_IP,
-    #     'port': int(WEBSOCKET_PORT)
-    # }
-    player = DQNPlayer(color=strp_color(PLAYER_COLOR))
-    # client = Client(player=player, settings=settings)
-    player.train()
-    # client.main()
-    # except Exception as e:
-    #     logger.error("An error occurred: %s", e)
+        settings = {
+            'current_state': INITIAL_STATE,
+            'timeout': int(TIMEOUT),
+            'server_ip': SERVER_IP,
+            'port': int(WEBSOCKET_PORT)
+        }
+        player = DQNPlayer(color=strp_color(PLAYER_COLOR))
+        client = Client(player=player, settings=settings)
+        client.main()
+    except Exception as e:
+        logger.error("An error occurred: %s", e)
